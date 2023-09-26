84c1d3c301020c0d327cb883c84b2f9dd31b45d9
Aldo A. Avalos <99375833+aldorithms@users.noreply.github.com>
2023-09-26 11:05:39 -0700

    rules and doas

--- src/doas.rs
+++ src/doas.rs
@@ -1,19 +1,7 @@
-//usage
-
-fn usage() {
+/**
+	@brief Called if arguments were incorrect, then exit the program.
+*/
+pub fn usage() {
     eprintln!("usage: doas [-nSs] [-a style] [-C config] [-u user] command [args]\n");
     std::process::exit(1);
 }
-
-/*
-static int parseuid(const char *s, uid_t *uid) {
-	struct passwd *pw;
-    pw = getpwnam(s);
-	if (pw != NULL) {
-		*uid = pw->pw_uid;
-		return 0;
-	}
-	return -1;
-}
-*/
-
