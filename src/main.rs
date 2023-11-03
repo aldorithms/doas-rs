0e3071609d66310304b5f2f19d52215a8cd9546a
Aldo A. Avalos <99375833+aldorithms@users.noreply.github.com>
2023-11-02 20:36:58 -0700

    fedgrgrw

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,9 @@
-use std::path::PathBuf;
+use std::{path::PathBuf, env::args};
 
-use clap::{arg, command, value_parser, ArgAction, Command};
+//use clap::{arg, command, value_parser, ArgAction, Command};
 
 fn main() {
     // usage: doas [-nSs] [-a style] [-C config] [-u user] command [args]
-    let matches = command!() // requires `cargo` feature
-        .arg(arg!(-L "Clear any persisted authentications from previous invocations, then exit."))
-        .arg(arg!(-n "Non interactive mode, fail if the matching rule doesn't have the nopass option."))
-        .arg(arg!(-s "Execute the shell from SHELL"))
-        // .arg(arg!(-a --style "Stylization"))
-        .arg(arg!(-C --config <FILE> "Sets a custom config file").required(false).value_parser(value_parser!(PathBuf)))
-        .arg(arg!(-u --user [user] "Run command as user, default is root"))
-        .arg(arg!(<command> ...).required(true));
+    let arguments : Vec<_> = args().skip(1).collect();
+    
 }
