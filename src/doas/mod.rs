1a03e6efce908b8ebf737c6771083c3cdd58c7fd
Aldo A. Avalos <99375833+aldorithms@users.noreply.github.com>
2023-11-14 19:42:07 -0800

    clapback

--- src/doas/mod.rs
+++
@@ -1,5 +0,0 @@
-mod rule;
-pub fn usage() {
-    eprintln!("usage: doas [-nSs] [-a style] [-C config] [-u user] command [args]\n");
-    std::process::exit(1);
-}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,49 @@
-use std::{path::PathBuf, env::args};
+use std::{path::PathBuf, error::Error};
 
-//use clap::{arg, command, value_parser, ArgAction, Command};
+use clap::{arg, value_parser, command};
+use libc::EILSEQ;
 
-fn main() {
-    // usage: doas [-nSs] [-a style] [-C config] [-u user] command [args]
-    let arguments : Vec<_> = args().skip(1).collect();
+mod doas;
+
+///
+/// Execute commands as another user
+/// 
+/// # Description
+/// 
+/// The doas utility executes the given command as another user. 
+/// The command argument is mandatory unless -C, -L, or -s is specified.
+/// 
+/// # Usage
+/// 
+/// doas [-nSs] [-a style] [-C config] [-u user] command [args]
+/// 
+/// # Arguments
+/// 
+/// `-C --config <FILE>`: 
+/// Parse and check the configuration file config, then exit. 
+/// If command is supplied, doas will also perform command matching.
+/// In the latter case either ‘permit’, ‘permit nopass’ or ‘deny’ will be printed on standard output, depending on command matching results. 
+/// No command is executed.
+/// 
+/// `-u --user`: Execute the command as user. The default is root.
+/// 
+/// # Examples
+/// 
+fn main() -> Result<(), Box<dyn Error + 'static>> {
+    let matches = command!()
+        .args(&[
+            arg!(Command: [command] ... "Command to run").required(false),
+            arg!(Config: -C --config <FILE>  "Specify config file to debug.").required(false).value_parser(value_parser!(PathBuf)),
+            arg!(Clear: -L --clear "Clear any persisted authentications from previous invocations.").required(false),
+            arg!(NoInteract: -n "Non-Interactive").required(false),
+            arg!(User: -u --user <USER> "Specify user to run command as.").required(false),
+            arg!(Shell: -s --shell "launch a shell as a user.").required(false),
+        ])
+        .get_matches();
+        
+        if matches.args_present() {
+            
+        }
     
+    Ok(())
 }
